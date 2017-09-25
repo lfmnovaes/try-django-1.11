43683b8191ab35347019a7e38a403a3fa217df40
lfmnovaes <lfmnovaes@gmail.com>
2017-09-25 17:12:17 -0300

    inheritance and bootstrap

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,20 +3,29 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-# def home(request):
-#     html_var = 'f strings'
-#     html_ = f"""<!DOCTYPE html>
-#     <html lang=en>
-#     <head>
-#     </head>
-#     <body>
-#     <h1>Hello World!</h1>
-#     <p>This is {html_var} coming through</p>
-#     </body>
-#     </html>
-#     """
-#   return HttpResponse(html_)
 
 def home(request):
-    num = random.randint(0, 1000000)
-    return render(request, "base.html", {"html_var": "context variable", "num": num})
+    num = None
+    some_list = [
+        random.randint(0, 1000000),
+        random.randint(0, 1000000),
+        random.randint(0, 1000000)
+    ]
+    condition_bool_item = True
+    if condition_bool_item:
+        num = random.randint(0, 1000000)
+    context = {
+        "num": num,
+        "some_list": some_list
+    }
+    return render(request, "home.html", context)
+
+def about(request):
+    context = {
+    }
+    return render(request, "about.html", context)
+
+def contact(request):
+    context = {
+    }
+    return render(request, "contact.html", context)
